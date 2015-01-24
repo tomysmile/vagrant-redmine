0c74bcb863aac29cc689337b8ace257392dbc305
Tomy Ismail <tomysmile@gmail.com>
2015-01-24 21:30:47 +0700

    Update provision

--- Vagrantfile
+++ Vagrantfile
@@ -5,8 +5,7 @@
 VAGRANTFILE_API_VERSION = "2"
 
 Vagrant.configure(VAGRANTFILE_API_VERSION) do |config|
-  config.vm.box = "ubuntu/trusty64"
-  config.vm.box_url = "https://cloud-images.ubuntu.com/vagrant/trusty/current/trusty-server-cloudimg-amd64-vagrant-disk1.box"
+  config.vm.box = "ubuntu/precise64"
   config.vm.hostname = "redmine-srvr"
 
   config.vm.network "forwarded_port", host: 8888, guest: 80, id: "redmine", auto_correct: true
@@ -14,6 +13,7 @@ Vagrant.configure(VAGRANTFILE_API_VERSION) do |config|
   config.vm.provision "shell", path: "provision.sh"
 
   config.vm.provider :virtualbox do |vb|
+    vb.name = "redmine-srvr"
     vb.customize ["modifyvm", :id, "--memory", "1024"]
   end
 
--- provision.sh
+++ provision.sh
@@ -1,138 +1,112 @@
 #!/usr/bin/env bash
+
 # Script to create a vagrant image with redmine ready to use.
-# @author Claudio Alarcon clalarco@gmail.com
 set -x
 
 # Uncomment the selected web server.
-USE_NGINX=1
-#USE_APACHE2=1
-#
+#USE_NGINX=1
+USE_APACHE2=1
 
 # Uncomment the selected database.
 USE_MYSQL=1
 #USE_PGSQL=1
-#USE_SQLITE3=1
 #
 # Uncomment if extra images libraries for redmine will be installed.
-#USE_IMAGEMAGICK=1
+USE_IMAGEMAGICK=1
 
 # Set password. Change at your preference.
-DB_PASSWORD='@Passw0rd'
-REDMINE_PASSWORD='@Passw0rd'
+DB_PASSWORD='1234567890'
+REDMINE_PASSWORD='1234567890'
 
 set +x
 
 # Set non-interactive instaler mode, update repos.
 export DEBIAN_FRONTEND=noninteractive
-sudo apt-get update
+
+if ! test -f .updated_apt_get; then
+  echo 'Updating and installing ubuntu packages...'
+  # Do actual update packages
+  apt-get -y update
+  touch .updated_apt_get
+fi
+
+dpkg -s git &>/dev/null || {
+  echo 'Installing supporting tools...'
+  apt-get -y install git curl build-essential
+}
 
 # Install and setup database.
 if [[ -n ${USE_MYSQL} ]]; then
   # Setup and install mysql-server
-  echo "redmine redmine/instances/default/database-type select mysql" | sudo debconf-set-selections
-  echo "redmine redmine/instances/default/mysql/method select unix socket" | sudo debconf-set-selections
-  echo "redmine redmine/instances/default/mysql/app-pass password ${DB_PASSWORD}" | sudo debconf-set-selections
-  echo "redmine redmine/instances/default/mysql/admin-pass password ${DB_PASSWORD}" | sudo debconf-set-selections
-  echo "mysql-server mysql-server/root_password password ${DB_PASSWORD}" | sudo debconf-set-selections
-  echo "mysql-server mysql-server/root_password_again password ${DB_PASSWORD}" | sudo debconf-set-selections
-  sudo apt-get install -q -y mysql-server mysql-client
-  sudo apt-get install -q -y redmine-mysql
+  dpkg -s mysql-server &>/dev/null || {
+    echo "redmine redmine/instances/default/database-type select mysql" | debconf-set-selections
+    echo "redmine redmine/instances/default/mysql/method select unix socket" | debconf-set-selections
+    echo "redmine redmine/instances/default/mysql/app-pass password ${DB_PASSWORD}" | debconf-set-selections
+    echo "redmine redmine/instances/default/mysql/admin-pass password ${DB_PASSWORD}" | debconf-set-selections
+    echo "mysql-server mysql-server/root_password password ${DB_PASSWORD}" | debconf-set-selections
+    echo "mysql-server mysql-server/root_password_again password ${DB_PASSWORD}" | debconf-set-selections
+    apt-get install -q -y mysql-server mysql-client
+    apt-get install -q -y redmine-mysql
+  }
 
 elif [[ -n ${USE_PGSQL} ]]; then
   # Setup and install pgsql-server
-  echo "redmine redmine/instances/default/database-type select pgsql" | sudo debconf-set-selections
-  echo "redmine redmine/instances/default/pgsql/method select unix socket" | sudo debconf-set-selections
-  echo "redmine redmine/instances/default/pgsql/authmethod-admin select ident" | sudo debconf-set-selections
-  echo "redmine redmine/instances/default/pgsql/authmethod-user select ident" | sudo debconf-set-selections
-  echo "redmine redmine/instances/default/pgsql/app-pass password" | sudo debconf-set-selections
-  echo "redmine redmine/instances/default/pgsql/admin-pass password" | sudo debconf-set-selections
-  echo "dbconfig-common dbconfig-common/pgsql/authmethod-admin select ident" | sudo debconf-set-selections
-  echo "dbconfig-common dbconfig-common/pgsql/authmethod-user select ident" | sudo debconf-set-selections
-  sudo apt-get install -q -y postgresql postgresql-contrib
-  sudo apt-get install -q -y redmine-pgsql
-
-elif [[ -n ${USE_SQLITE3} ]]; then
-  echo 'redmine redmine/instances/default/database-type select sqlite3' | sudo debconf-set-selections
-  echo 'redmine redmine/instances/default/db/basepath string /var/lib/dbconfig-common/sqlite3/redmine/instances/default' | sudo debconf-set-selections
-  # Sqlite is installed and used by default as database if no other is set.
+  dpkg -s postgresql &>/dev/null || {
+    echo "redmine redmine/instances/default/database-type select pgsql" | debconf-set-selections
+    echo "redmine redmine/instances/default/pgsql/method select unix socket" | debconf-set-selections
+    echo "redmine redmine/instances/default/pgsql/authmethod-admin select ident" | debconf-set-selections
+    echo "redmine redmine/instances/default/pgsql/authmethod-user select ident" | debconf-set-selections
+    echo "redmine redmine/instances/default/pgsql/app-pass password" | debconf-set-selections
+    echo "redmine redmine/instances/default/pgsql/admin-pass password" | debconf-set-selections
+    echo "dbconfig-common dbconfig-common/pgsql/authmethod-admin select ident" | debconf-set-selections
+    echo "dbconfig-common dbconfig-common/pgsql/authmethod-user select ident" | debconf-set-selections
+    apt-get install -q -y postgresql postgresql-contrib
+    apt-get install -q -y redmine-pgsql
+  }
 fi
 
 # Install redmine.
-echo "redmine redmine/instances/default/app-password password ${REDMINE_PASSWORD}" | sudo debconf-set-selections
-echo "redmine redmine/instances/default/app-password-confirm password ${REDMINE_PASSWORD}" | sudo debconf-set-selections
-echo "redmine redmine/instances/default/dbconfig-install boolean true" | sudo debconf-set-selections
-sudo apt-get install -q -y redmine
+dpkg -s redmine &>/dev/null || {
+  echo "redmine redmine/instances/default/app-password password ${REDMINE_PASSWORD}" | debconf-set-selections
+  echo "redmine redmine/instances/default/app-password-confirm password ${REDMINE_PASSWORD}" | debconf-set-selections
+  echo "redmine redmine/instances/default/dbconfig-install boolean true" | debconf-set-selections
+  apt-get install -q -y redmine
 
-# Extra required package for ubuntu 14.04 to make redmine work.
-sudo gem install bundler
+  # Extra required package for ubuntu 14.04 to make redmine work.
+  gem install bundler
 
-# Extras
-if [[ -n ${USE_IMAGEMAGICK} ]]; then
-  sudo apt-get install -q -y imagemagick
-  sudo apt-get install -q -y ruby-rmagick
-fi
+  # Extras
+  if [[ -n ${USE_IMAGEMAGICK} ]]; then
+    apt-get install -q -y imagemagick
+    apt-get install -q -y ruby-rmagick
+  fi
+}
 
 # Change permissions for redmine directory.
-sudo chown www-data:www-data /usr/share/redmine
+chown www-data:www-data /usr/share/redmine
+
 #############################################
 # Install web servers.
 #############################################
-# nginx as first option.
-# ----------------------
-if [[ -n ${USE_NGINX} ]]; then
-  # nginx and thin are required.
-  sudo apt-get install -q -y nginx thin
+# Install apache2
+dpkg -s apache2 &>/dev/null || {
+  apt-get install -q -y apache2 libapache2-mod-passenger
 
-  # Configure thin.
-  sudo thin config \
-  --config /etc/thin1.9.1/redmine.yml \
-  --chdir /usr/share/redmine \
-  --environment production \
-  --servers 2 \
-  --socket /tmp/thin.redmine.sock \
-  --pid tmp/pids/thin.pid
-
-  # Configure nginx. For now default config is overriden.
-  sudo dd of=/etc/nginx/sites-available/default << EOF
-
-  upstream redmine_upstream {
-    server unix:/tmp/thin.redmine.0.sock;
-    server unix:/tmp/thin.redmine.1.sock;
-  }
-
-  server {
-    listen 80;
-    server_name 127.0.0.1;
-    root /usr/share/redmine/public;
-    location / {
-    try_files \$uri @redmine_ruby;
-  }
-    location @redmine_ruby {
-      proxy_set_header X-Real-IP \$remote_addr;
-      proxy_set_header X-Forwarded-For \$proxy_add_x_forwarded_for;
-      proxy_set_header Host \$http_host;
-      proxy_redirect off;
-      proxy_read_timeout 300;
-      proxy_pass http://redmine_upstream;
-    }
-  }
-  EOF
-
-  # Restart thin
-  sudo service thin restart
-
-  # Restart nginx
-  sudo service nginx restart
-
-  # Else, install apache2.
-  # ----------------------
-elif [[ -n ${USE_APACHE2} ]]; then
-  # Install apache2
-  sudo apt-get install -q -y apache2 libapache2-mod-passenger
   # Link redmine into apache2.
-  sudo ln -s /usr/share/redmine/public /var/www/redmine
+  ln -s /usr/share/redmine/public /var/www/redmine
+}
+
+if ! test -f .apache2_configured; then
+
+  if test -f /etc/apache2/sites-available/default; then
+    touch /etc/apache2/sites-available/000-default.conf
+
+    rm -f /etc/apache2/sites-enabled/000-default
+    ln -s /etc/apache2/sites-available/000-default.conf /etc/apache2/sites-enabled/000-default
+  fi
+
   # Override apache settings.
-  sudo dd of=/etc/apache2/sites-available/000-default.conf <<EOF
+  dd of=/etc/apache2/sites-available/000-default.conf <<EOF
   <VirtualHost *:80>
     ServerAdmin webmaster@localhost
     DocumentRoot /var/www/redmine
@@ -146,22 +120,35 @@ elif [[ -n ${USE_APACHE2} ]]; then
 EOF
 
   # Configure passenger
-  sudo dd of=/etc/apache2/mods-available/passenger.conf <<EOF
+  dd of=/etc/apache2/mods-available/passenger.conf <<EOF
   <IfModule mod_passenger.c>
     PassengerDefaultUser www-data
-    PassengerRoot /usr/lib/ruby/vendor_ruby/phusion_passenger/locations.ini
-    PassengerDefaultRuby /usr/bin/ruby
+    PassengerRoot /usr
+    PassengerRuby /usr/bin/ruby
   </IfModule>
 EOF
 
-  # Configure security messages.
-  sed -i 's|Server Tokens .*|Server Tokens Prod|g' /etc/apache2/conf-available/security.conf
-  # Restart apache2
-  sudo service apache2 restart
-  fi
+
+  touch .apache2_configured
+fi
+
+# Restart apache2
+service apache2 restart
+
+dpkg -s subversion &>/dev/null || {
+  echo 'Installing subversion...'
+  apt-get -y install subversion cvs mercurial
+}
+
+dpkg -s postfix &>/dev/null || {
+  echo 'Installing postfix mail...'
+  apt-get -y install postfix
+}
+
   cat <<EOF
   ################################################
   # Now you should be able to see redmine webpage
   # http://localhost:8888
   ################################################
 EOF
+
